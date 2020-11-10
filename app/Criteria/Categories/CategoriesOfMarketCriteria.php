abf56a7a5153d0bf5fd9da6042f756eab83e4723
INDRO NIL RAY <indro207902@gmail.com>
2020-11-10 23:08:39 +0600

    first commit

---
+++ app/Criteria/Categories/CategoriesOfMarketCriteria.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Criteria\Categories;
+
+use DB;
+use Illuminate\Http\Request;
+use Prettus\Repository\Contracts\CriteriaInterface;
+use Prettus\Repository\Contracts\RepositoryInterface;
+
+/**
+ * Class CategoriesOfMarketCriteria.
+ *
+ * @package namespace App\Criteria\Categories;
+ */
+class CategoriesOfMarketCriteria implements CriteriaInterface
+{
+    /**
+     * @var array
+     */
+    private $request;
+
+    /**
+     * CategoriesOfMarketCriteria constructor.
+     */
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    /**
+     * Apply criteria in query repository
+     *
+     * @param string              $model
+     * @param RepositoryInterface $repository
+     *
+     * @return mixed
+     */
+    public function apply($model, RepositoryInterface $repository)
+    {
+        if (!$this->request->has('market_id')) {
+            return $model;
+        } else {
+            $id = $this->request->get('market_id');
+            // return $model->join('products', 'products.category_id', '=', 'categories.id')
+            //     ->where('products.market_id', $id)
+            //     ->select('categories.*')
+            //     ->groupBy('categories.id');
+            // dd($model['productcount']);
+            return $model->join('products', 'products.category_id', '=', 'categories.id')
+                ->where('products.market_id', $id)
+                ->select(array('categories.*', DB::raw('COUNT(products.category_id) as marketproducts')))
+                // ->select('categories.*')
+                ->groupBy('categories.id');
+            
+            // return 0;
+        }
+    }
+}
